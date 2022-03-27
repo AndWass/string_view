f86b9fe3b21421e503eb336d6ed425213fbfdd2d
Andreas Wass <wass.andreas@gmail.com>
2022-03-27 19:56:47 +0200

    Add license

--- include/andwass/string_view.hpp
+++ include/andwass/string_view.hpp
@@ -1,8 +1,15 @@
+//          Copyright Andreas Wass 2022
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+
 #pragma once
 #include <cstring>
 #include <cctype>
 #include <cmath>
 #include <algorithm>
+#include <stdexcept>
 
 namespace andwass {
 namespace detail
